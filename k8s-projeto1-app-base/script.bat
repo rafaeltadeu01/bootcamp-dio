d56829984f308a8f1174f22ca1aa8ee78e9dacb6
Rafael Tadeu <rafael.almeida@tempoassist.com.br>
2023-01-27 20:29:44 -0300

    Projeto do K8S

---
+++ k8s-projeto1-app-base/script.bat
@@ -0,0 +1,18 @@
+echo "Criando as imagens........."
+
+docker build -t rafaeltadeu01/projeto-backend:1.0 backend/.
+docker build -t rafaeltadeu01/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens........."
+
+docker push rafaeltadeu01/projeto-backend:1.0 
+docker push rafaeltadeu01/projeto-database:1.0
+
+echo "Criando servico no Cluster Kubernetes........."
+
+kubctl apply -f ./services.yml
+
+echo "Criando os deploymets........."
+
+kubctl apply -f ./deployment.yml
+
